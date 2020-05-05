4268bd3d677f588d9ffd21da07e288adb5958fec
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-05-06 00:01:40 +0100

    add other topologies

---
+++ src/stk/molecular/topology_graphs/metal_complex/paddlewheel/paddlewheel.py
@@ -0,0 +1,89 @@
+"""
+Paddlewheel
+===========
+
+"""
+
+from ..metal_complex import MetalComplex
+from ..vertices import _MetalVertex, _BiDentateLigandVertex
+from ...topology_graph import Edge
+
+
+class Paddlewheel(MetalComplex):
+    """
+    Represents a metal complex topology graph.
+
+    Metal building blocks with at least 4 functional groups are
+    required for this topology.
+
+    Ligand building blocks with two functional groups are required for
+    this topology.
+
+    See :class:`.MetalComplex` for more details and examples.
+
+    """
+
+    _metal_vertex_prototypes = (
+        _MetalVertex(0, [0, 1, 0]),
+        _MetalVertex(1, [0, -1, 0]),
+    )
+    _ligand_vertex_prototypes = (
+        _BiDentateLigandVertex(1, [2, 0, 0]),
+        _BiDentateLigandVertex(2, [0, 0, 2]),
+        _BiDentateLigandVertex(3, [-2, 0, 0]),
+        _BiDentateLigandVertex(4, [0, 0, -2]),
+    )
+
+    _edge_prototypes = (
+        Edge(
+            id=0,
+            vertex1=_metal_vertex_prototypes[0],
+            vertex2=_ligand_vertex_prototypes[0],
+            position=[0.1, 0.5, 0],
+        ),
+        Edge(
+            id=1,
+            vertex1=_metal_vertex_prototypes[1],
+            vertex2=_ligand_vertex_prototypes[0],
+            position=[0.1, -0.5, 0],
+        ),
+
+        Edge(
+            id=2,
+            vertex1=_metal_vertex_prototypes[0],
+            vertex2=_ligand_vertex_prototypes[1],
+            position=[0, 0.5, 0.1],
+        ),
+        Edge(
+            id=3,
+            vertex1=_metal_vertex_prototypes[1],
+            vertex2=_ligand_vertex_prototypes[1],
+            position=[0, -0.5, 0.1],
+        ),
+
+        Edge(
+            id=4,
+            vertex1=_metal_vertex_prototypes[0],
+            vertex2=_ligand_vertex_prototypes[2],
+            position=[-0.1, 0.5, 0],
+        ),
+        Edge(
+            id=5,
+            vertex1=_metal_vertex_prototypes[1],
+            vertex2=_ligand_vertex_prototypes[2],
+            position=[-0.1, -0.5, 0],
+        ),
+
+        Edge(
+            id=6,
+            vertex1=_metal_vertex_prototypes[0],
+            vertex2=_ligand_vertex_prototypes[3],
+            position=[0, 0.5, -0.1],
+        ),
+        Edge(
+            id=7,
+            vertex1=_metal_vertex_prototypes[1],
+            vertex2=_ligand_vertex_prototypes[3],
+            position=[0, -0.5, -0.1],
+        ),
+    )
