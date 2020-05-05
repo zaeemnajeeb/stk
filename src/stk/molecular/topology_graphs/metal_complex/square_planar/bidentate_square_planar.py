a372dca921cfab0c73f891b3d008f5fd7fee8dfb
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-05-06 00:01:23 +0100

    add square planar topologies

---
+++ src/stk/molecular/topology_graphs/metal_complex/square_planar/bidentate_square_planar.py
@@ -0,0 +1,60 @@
+"""
+Bidentate Square Planar
+=============
+
+"""
+
+from ..metal_complex import MetalComplex
+from ..vertices import _MetalVertex, _BiDentateLigandVertex
+from ...topology_graph import Edge
+
+
+class BidentateSquarePlanar(MetalComplex):
+    """
+    Represents a square planar metal complex topology graph.
+
+    Metal building blocks with at least four functional groups are
+    required for this topology graph.
+
+    Linker building blocks with two functional groups are required
+    for this topology graph.
+
+    See :class:`.MetalComplex` for more details and examples.
+
+    """
+
+    _metal_vertex_prototypes = (
+        _MetalVertex(0, [0, 0, 0]),
+    )
+    _ligand_vertex_prototypes = (
+        _BiDentateLigandVertex(1, [1, 1, 0]),
+        _BiDentateLigandVertex(2, [-1, -1, 0]),
+    )
+
+    _edge_prototypes = (
+        Edge(
+            id=0,
+            vertex1=_metal_vertex_prototypes[0],
+            vertex2=_ligand_vertex_prototypes[0],
+            position=[1, 0, 0],
+        ),
+        Edge(
+            id=1,
+            vertex1=_metal_vertex_prototypes[0],
+            vertex2=_ligand_vertex_prototypes[0],
+            position=[0, 1, 0],
+        ),
+
+        Edge(
+            id=2,
+            vertex1=_metal_vertex_prototypes[0],
+            vertex2=_ligand_vertex_prototypes[1],
+            position=[-1, 0, 0],
+        ),
+        Edge(
+            id=3,
+            vertex1=_metal_vertex_prototypes[0],
+            vertex2=_ligand_vertex_prototypes[1],
+            position=[0, -1, 0],
+        ),
+    )
