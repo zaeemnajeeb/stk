f46d8047a2dc24f699bc959af2306b8d49c447a0
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-05-07 16:37:22 +0100

    Add BentLinkerVertex

--- src/stk/molecular/topology_graphs/cage/vertices.py
+++ src/stk/molecular/topology_graphs/cage/vertices.py
@@ -263,3 +263,34 @@ class _NonLinearCageVertex(_CageVertex):
                 edge_sorter.get_items(),
             )
         }
+
+
+class _BentMetalComplexCageVertex(_CageVertex):
+    def place_building_block(self, building_block, edges):
+        building_block = building_block.with_centroid(
+            position=self._position,
+            atom_ids=building_block.get_placer_ids(),
+        )
+        edge_centroid = (
+            sum(edge.get_position() for edge in edges) / len(edges)
+        )
+        core_centroid = building_block.get_centroid(
+            atom_ids=building_block.get_core_atom_ids(),
+        )
+        return building_block.with_rotation_between_vectors(
+            start=core_centroid - self._position,
+            target=self._position - edge_centroid,
+            origin=self._position,
+        ).get_position_matrix()
+
+    def map_functional_groups_to_edges(self, building_block, edges):
+        fg, = building_block.get_functional_groups(0)
+        fg_position = building_block.get_centroid(fg.get_placer_ids())
+
+        def fg_distance(edge):
+            return euclidean(edge.get_position(), fg_position)
+
+        edges = sorted(edges, key=fg_distance)
+        return {
+            fg_id: edge.get_id() for fg_id, edge in enumerate(edges)
+        }
