dba9a252df84be0e832d736cb785e12830765c28
Andrew Tarzia <andrew.tarzia@gmail.com>
2020-05-11 09:30:08 +0100

    Change name

--- src/stk/molecular/topology_graphs/cage/metal_topologies/__init__.py
+++ src/stk/molecular/topology_graphs/cage/metal_topologies/__init__.py
@@ -2,7 +2,7 @@ from .m2l4_lantern import *  # noqa
 from .m3l3_triangle import *  # noqa
 from .m3l6 import *  # noqa
 from .m4l4_square import *  # noqa
-from .m4l4_tetrahedron_spacer import *  # noqa
+from .m4l4_tetrahedron import *  # noqa
 from .m4l6_tetrahedron import *  # noqa
 from .m4l6_tetrahedron_spacer import *  # noqa
 from .m4l8 import *  # noqa
